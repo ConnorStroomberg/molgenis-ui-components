decce80809cc3d90847f99bed1f6d373e8908aca
Mark-de-Haan <markdehaan90@gmail.com>
2017-12-14 13:47:41 +0100

    Update footer to bootstrap beta

--- footer/src/main.js
+++ footer/src/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
-
 import Footer from './Footer'
 
-import 'bootstrap/dist/css/bootstrap.css' // Bootstrap import
 import 'font-awesome/css/font-awesome.min.css' // Font awesome import
+
 /* eslint-disable no-new */
 new Vue({
   el: '#footer',
