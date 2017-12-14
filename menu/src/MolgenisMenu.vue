1a9bbdf513038c9abe7e7376d859d0ca104efd7b
Mark-de-Haan <markdehaan90@gmail.com>
2017-12-14 16:15:56 +0100

    Fix navbar for bootstrap4 beta

--- menu/src/MolgenisMenu.vue
+++ menu/src/MolgenisMenu.vue
@@ -1,70 +1,81 @@
 <template>
-  <div class="fixed-top">
-    <header v-if="topLogo" id="top-logo-vue-banner">
+  <header class="fixed-top">
+    <div v-if="topLogo" id="top-logo-vue-banner">
       <a href="/"><img id="logo-top" :src="topLogo" alt="" border="0"></a>
-    </header>
-    <nav class="navbar navbar-expand-md navbar-light bg-faded">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-              aria-controls="navbarNavDropdown" aria-expanded="false"
-              aria-label="Toggle navigation" v-on:click="toggleNavbar">
+    </div>
+
+    <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+      <a v-if="navBarLogo" class="navbar-brand" :href="'/menu/main/'+menu.items[0].href">
+        <img :src="navBarLogo" height="30">
+      </a>
+      <a v-else class="navbar-brand" href="#"></a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content"
+              aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <a v-if="navBarLogo" class="navbar-brand" :href="'/menu/main/'+menu.items[0].href"><img
-        :src="navBarLogo" height="30"></a>
-      <a v-else class="navbar-brand" href="#"></a>
-
-      <div :class="{collapse:collapse, 'navbar-collapse':collapse}" id="navbarNavDropdown">
+      <div class="collapse navbar-collapse" id="navbar-content">
         <ul class="navbar-nav mr-auto">
           <template v-for="(item, index) in menu.items">
+            <li v-if="item.type === 'PLUGIN'"
+                :class="['nav-item', {'active': isSelectedPlugin(item.id)}]">
 
-            <template v-if="item.type == 'PLUGIN' && (index != 0 || !navBarLogo)">
-              <!--index == 0 AND toplogo is set: show logo-->
-              <li class="nav-item" :class="{active:isSelectedPlugin(item.id)}">
-                <a class="nav-link" :href="'/menu/main/'+item.href">{{item.label}}</a>
-              </li>
-            </template>
-            <template v-else>
-              <template v-if="item.type == 'MENU' && index != 0">
-                <sub-menu :id="item.id" :label="item.label" :index="index"
-                          :selected="index == selectedIndex" :callback="setIndex"
-                          :items="item.items"></sub-menu>
-              </template>
-            </template>
+              <a class="nav-link" :href="'/menu/main/'+item.href">
+                {{ item.label }}
+              </a>
+            </li>
+
+            <li class="nav-item dropdown" v-else>
+              <a class="nav-link dropdown-toggle" :id="item.id"
+                 role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {{ item.label }}
+              </a>
+              <drop-down-items :parent="item" :items="item.items"></drop-down-items>
+            </li>
           </template>
         </ul>
-        <!--right side menu items-->
-        <ul class="nav navbar-nav">
+
+        <ul class="navbar-nav float-right">
           <li class="nav-item">
-            <!--Add language dropdown-->
-            <div id="language-select-box" class="navbar-right"></div>
+            <div id="language-select-box"></div>
           </li>
+
           <li class="nav-item">
-            <a class="nav-link navbar-right" :href=helpLink.href
-               target='_blank'>{{helpLink.label}}</a>
+            <a class="nav-link" :href=helpLink.href target='_blank'>
+              {{ helpLink.label }}
+            </a>
           </li>
+
           <li v-if="authenticated" class="nav-link">
-            <form id="logout-form" class="navbar-form navbar-right" method="post" action="/logout">
-              <button id="signout-button" type="button" class="btn btn-primary btn-sm" v-on:click="logout">Sign out
+            <form id="logout-form" class="navbar-form" method="post" action="/logout">
+              <button id="signout-button" type="button" class="btn btn-light" @click="logout">
+                Sign out
               </button>
             </form>
           </li>
+
           <li v-else class="nav-item">
-            <a class='nav-link navbar-right btn btn-secondary' v-on:click="login">Sign in</a>
+            <a class='nav-link btn btn-light' @click="login">Sign in</a>
           </li>
         </ul>
       </div>
     </nav>
-  </div>
+  </header>
 </template>
 
+<style>
+  .dropdown-toggle:hover {
+    cursor: pointer;
+  }
+</style>
+
 <script>
-  import SubMenu from './SubMenu'
+  import DropDownItems from './DropDownItems'
 
   export default {
-    components: {SubMenu},
     name: 'molgenis-menu',
-    data: function () {
+    data () {
       return {
         menu: window.molgenisMenu.menu,
         topLogo: window.molgenisMenu.topLogo,
@@ -74,43 +85,27 @@
         selectedPlugin: window.molgenisMenu.selectedPlugin,
         showLanguageDropdown: window.molgenisMenu.showLanguageDropdown,
         logoutFunction: window.molgenisMenu.logoutFunction,
-        googleSignIn: window.molgenisMenu.googleSignIn,
-        collapse: true,
-        selectedIndex: {
-          default: -1,
-          type: Number
-        }
+        googleSignIn: window.molgenisMenu.googleSignIn
       }
     },
     methods: {
-      setIndex: function (index) {
-        if (this.selectedIndex === index) {
-          this.selectedIndex = -1
-        } else {
-          this.selectedIndex = index
-        }
-      },
-      toggleNavbar: function () {
-        this.collapse = !this.collapse
-      },
-      isSelectedPlugin: function (plugin) {
+      isSelectedPlugin (plugin) {
         return plugin === this.selectedPlugin
       },
-      logout: function () {
+      logout () {
         if (this.logoutFunction) {
           this.logoutFunction()
         }
         document.getElementById('logout-form').submit()
       },
-      login: function () {
+      login () {
         const modal = document.getElementById('login-modal')
         modal.classList.add('show')
         modal.style.display = 'inline'
       }
     },
-    mounted: function () {
-      let setIndex = this.setIndex
-      window.addEventListener('click', function (e) { if (e.target.className !== 'nav-link dropdown-toggle') { setIndex(-1) } }, false)
+    components: {
+      DropDownItems
     }
   }
 </script>
--- menu/src/main.js
+++ menu/src/main.js
@@ -2,9 +2,7 @@ import 'es6-promise/auto'
 import 'babel-polyfill'
 
 import Vue from 'vue'
-
 import MolgenisMenu from './MolgenisMenu'
-
 import 'font-awesome/css/font-awesome.min.css' // Font awesome import
 
 /* eslint-disable no-new */
