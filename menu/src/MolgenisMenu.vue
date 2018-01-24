642b5a31143442cad463a4020cb1dbb719c8a9cd
Mark-de-Haan <markdehaan90@gmail.com>
2018-01-24 12:18:19 +0100

    Add language select box

--- menu/src/MolgenisMenu.vue
+++ menu/src/MolgenisMenu.vue
@@ -5,61 +5,70 @@
     </div>
 
     <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
-        <a v-if="navBarLogo" class="navbar-brand" :href="'/menu/main/'+menu.items[0].href">
-          <img :src="navBarLogo" height="30">
-        </a>
-        <a v-else class="navbar-brand" href="#"></a>
+      <a v-if="navBarLogo" class="navbar-brand" :href="'/menu/main/'+menu.items[0].href">
+        <img :src="navBarLogo" height="30">
+      </a>
+      <a v-else class="navbar-brand" href="#"></a>
 
-        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar-content"
-                aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar-content"
+              aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <div class="collapse navbar-collapse" id="navbar-content">
-          <ul class="navbar-nav mr-auto">
+      <div class="collapse navbar-collapse" id="navbar-content">
+        <ul class="navbar-nav mr-auto">
 
-            <template v-for="item in menu.items">
-              <li v-if="item.type === 'PLUGIN' && item.id !== 'home'"
-                  :class="['nav-item', {'active': isSelectedPlugin(item.id)}]">
+          <template v-for="item in menu.items">
+            <li v-if="item.type === 'PLUGIN' && item.id !== 'home'"
+                :class="['nav-item', {'active': isSelectedPlugin(item.id)}]">
 
-                <a class="nav-link" :href="'/menu/main/'+item.href">
-                  {{ item.label }}
-                </a>
-              </li>
-
-              <li v-else-if="item.id !== 'home'" class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" :id="item.id" data-toggle="dropdown" aria-haspopup="true"
-                   aria-expanded="false">
-                  {{ item.label }}
-                </a>
-                <drop-down-items :parent="item" :items="item.items"/>
-              </li>
-            </template>
-
-          </ul>
-
-          <ul class="navbar-nav justify-content-end">
-            <li class="nav-item">
-              <div id="language-select-box"></div>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" :href="helpLink.href" target="_blank">
-                {{ helpLink.label }}
+              <a class="nav-link" :href="'/menu/main/'+item.href">
+                {{ item.label }}
               </a>
             </li>
 
-            <li class="nav-item">
-              <form id="logout-form" class="navbar-form" method="post" action="/logout">
-                <button v-if="authenticated" id="signout-button" type="button" class="btn btn-outline-secondary" @click="logout">
-                  Sign out
-                </button>
-
-                <button v-else class='btn btn btn-outline-primary' type="button" @click="login">Sign in</button>
-              </form>
+            <li v-else-if="item.id !== 'home'" class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" :id="item.id" data-toggle="dropdown" aria-haspopup="true"
+                 aria-expanded="false">
+                {{ item.label }}
+              </a>
+              <drop-down-items :parent="item" :items="item.items"/>
             </li>
-          </ul>
-        </div>
+          </template>
+
+        </ul>
+
+        <ul class="navbar-nav justify-content">
+          <li v-if="authenticated" class="nav-item">
+            <form id="language-form" class="navbar-form">
+              <select class="nav-link" v-model="selectedLanguage" @change="handleLanguageSelect">
+                <option v-for="language in languages" :value="language.id" :selected="language.id === selectedLanguage">
+                  {{ language.label }}
+                </option>
+              </select>
+            </form>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link" :href="helpLink.href" target="_blank">
+              {{ helpLink.label }}
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <form id="logout-form" class="navbar-form" method="post" action="/logout">
+              <button v-if="authenticated" id="signout-button" type="button" class="btn btn-outline-secondary"
+                      @click="logout">
+                Sign out
+              </button>
+
+              <button v-else class='btn btn btn-outline-primary' type="button" @click="login">Sign in</button>
+            </form>
+          </li>
+        </ul>
+      </div>
     </nav>
   </header>
 </template>
@@ -67,10 +76,14 @@
 <script>
   import DropDownItems from './DropDownItems'
 
+  import api from '@molgenis/molgenis-api-client'
+
   export default {
     name: 'molgenis-menu',
     data () {
       return {
+        selectedLanguage: null,
+        languages: [],
         menu: window.molgenisMenu.menu,
         topLogo: window.molgenisMenu.topLogo,
         navBarLogo: window.molgenisMenu.navBarLogo,
@@ -96,6 +109,24 @@
         const modal = document.getElementById('login-modal')
         modal.classList.add('show')
         modal.style.display = 'inline'
+      },
+      handleLanguageSelect () {
+        api.post('/plugin/useraccount/language/update?languageCode=' + this.selectedLanguage).then(() => {
+          location.reload(true)
+        })
+      }
+    },
+    mounted () {
+      if (this.authenticated) {
+        api.get('/api/v2/sys_Language?q=active==true').then(response => {
+          this.languages = response.items.map(item => {
+            const language = {id: item.code, label: item.name}
+            if (item.code === response.meta.languageCode) {
+              this.selectedLanguage = language.id
+            }
+            return language
+          })
+        })
       }
     },
     components: {
